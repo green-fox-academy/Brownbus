5100244bedbbfaf3df539dd5f49b6d2910d6040a
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-17 15:48:28 +0100

    bmi excercise is up as 9ű

---
+++ week-01/day-4/excercise9.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+let a: number = heightInM * heightInM; 
+console.log(massInKg / a);
+
+// Print the Body mass index (BMI) based on these values
