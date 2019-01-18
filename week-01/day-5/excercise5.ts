810f46d29fd44634217a4df296ac4d17287be5ad
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-18 12:39:34 +0100

    triangle excercise up

---
+++ week-01/day-5/excercise5.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+let lineCount: number = 4;
+//DARW TRIANGLE
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+let a: string = '*';
+
+for (
+let i: number=0; lineCount>i; i++){
+
+    console.log(a);
+    a = a+"*";
+    
+
+
+}
