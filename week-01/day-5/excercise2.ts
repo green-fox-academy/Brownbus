22318dca3c3a2f98bee8c408eed491a368e7835e
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-18 12:03:11 +0100

    count to 500 exc up

---
+++ week-01/day-5/excercise2.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+let a: number = 500;
+
+for (let i:number = 0; i<500; i++){
+    if (i%2 === 0){
+        console.log(i);
+    }
+ }
+
+ //and another one
+
+for(let i: number = 0; i<=a ;i+=2){    
+    console.log(i);
+    
+    }
+
