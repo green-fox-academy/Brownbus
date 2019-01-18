4fb0764ffc327c4bd08c53865913c2466b8ad4fe
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-18 12:29:59 +0100

    Fizz-Buzz excercise is up

---
+++ week-01/day-5/excercise4.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+for(let i:number=0; i<=100; i++){
+if( i%3===0 && i%5 === 0 ){
+        console.log(i + "FizzBuzz")
+
+    }else if(i%5===0){
+        console.log(i + "Buzz")
+
+    }else if(i%3===0){
+        console.log( i + "Fizz")
+
+    }else{
+    console.log(i)
+}
+
+}
