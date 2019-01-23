4dc7090b48d9227f5cd3a609a02850c9cde0b195
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-23 19:14:35 +0100

    add greys to Starry night

--- week-02/day-3/starry_night/starry.ts
+++ week-02/day-3/starry_night/starry.ts
@@ -17,12 +17,21 @@ function getRandom(max) {
 ctx.fillStyle = 'black';
 ctx.fillRect(0,0,600,400);
 
+function grey(){
+  let x: string = getRandom(200).toString()
+  let y: string = 'rgb(' + x.concat(', ', x, ', ', x, ' )');
+  return y;
+}
+
 
 function stars(x){
 for(let i: number = 0; i<= x; i++){
-    ctx.fillStyle = 'white';
-    ctx.fillRect(getRandom(600), getRandom(400), 3, 3);
+    ctx.fillStyle = grey();
+    ctx.fillRect(getRandom(600), getRandom(400), 2, 2);
     }
 }
 
-stars(150)
+
+
+
+stars(250)
