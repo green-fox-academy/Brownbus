06e034d604261f61d3dd5fd647cef618e54b1109
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-24 16:47:10 +0100

    push

--- week-02/day-4/lineplay_quarters/line_play_quarters.ts
+++ week-02/day-4/lineplay_quarters/line_play_quarters.ts
@@ -2,53 +2,40 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-
-//let m: number = 0;
-
-
-/* function linePlay(s) {
-  for (let i: number = 0; i < s / 10; i++) {
-    ctx.beginPath();
-    ctx.strokeStyle = 'purple';
-    ctx.moveTo(s, s - i * 10 - s * 0.1);
-    ctx.lineTo(s - i*10, 0);
-    ctx.stroke();
-  }
-  for (let i: number = 0; i < s / 10; i++) {
-    ctx.beginPath();
-    ctx.strokeStyle = 'green';
-    ctx.moveTo( s - i * 10 - s * 0.1, s);
-    ctx.lineTo(0, s - i*10, );
-    ctx.stroke();
-  }
-} */
+//let size: number = 400;
+function row(s, m, c, y) {
   
-function div(s, t){
-for(let i: number = 0; i<t; i++){
-for (let i: number = 0; i < s/t / 10; i++) {
-  ctx.beginPath();
-  ctx.strokeStyle = 'purple';
-  ctx.moveTo(s/t, s/t - i * 10 - s/t * 0.1);
-  ctx.lineTo(s/t - i*10+t*i, 0);
-  ctx.stroke();
+  
+  
+  let x: number = s / m;
+  for (let j: number = 0; j < m; j++) {
+    for (let i: number = 0; i < x / 10; i++) {
+      ctx.beginPath();
+      ctx.strokeStyle = 'purple';
+      ctx.moveTo(c + x + j * x, y + x - i * 10 - x * 0.1);
+      ctx.lineTo(c + x + j * x - i * 10, y+0);
+      ctx.stroke();
+    }
+    for (let i: number = 0; i < x / 10; i++) {
+      ctx.beginPath();
+      ctx.strokeStyle = 'green';
+      ctx.moveTo(c + x - i * 10 - x * 0.1 + j * x, y + x);
+      ctx.lineTo(c + j * x,y + x - i * 10);
+      ctx.stroke();
+    }
+  }
+}
+//row(400, 4,0,100)
+
+function linePlay(s,q){
+
+ let y: number = 0;
+ let x: number = 0;
+ 
+ for(let r: number = 0; r<q; r++){
+ row(s,q,x,y);
+ y += s/q
+}
 
 }
-for (let i: number = 0; i < s / 10; i++) {
-  ctx.beginPath();
-  ctx.strokeStyle = 'green';
-  ctx.moveTo( s/t - i * 10 - s/t * 0.1, s/t);
-  ctx.lineTo(0, s/t - i*10);
-  ctx.stroke();
-}
-}
-}
-
-div(400,1)
-
-
-
-
-
-
-
-//linePlay(400)
+linePlay(400,64)
