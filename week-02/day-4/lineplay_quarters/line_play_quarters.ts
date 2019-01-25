9b01f7a8928345d4d0adf3d5fd87c7082ec8cc86
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-01-25 09:36:16 +0100

    line play quarters redone

--- week-02/day-4/lineplay_quarters/line_play_quarters.ts
+++ week-02/day-4/lineplay_quarters/line_play_quarters.ts
@@ -3,38 +3,38 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 //let size: number = 400;
-function row(s, m, c, y) {
+function row(lineDensity,canvasSize, divider, xCoord, yCoord) {
   
   
   
-  let x: number = s / m;
-  for (let j: number = 0; j < m; j++) {
-    for (let i: number = 0; i < x / 10; i++) {
+  let x: number = canvasSize / divider;
+  for (let j: number = 0; j < divider; j++) {
+    for (let i: number = 0; i < x / lineDensity; i++) {
       ctx.beginPath();
       ctx.strokeStyle = 'purple';
-      ctx.moveTo(c + x + j * x, y + x - i * 10 - x * 0.1);
-      ctx.lineTo(c + x + j * x - i * 10, y+0);
+      ctx.moveTo(xCoord + x + j * x, yCoord + x - i * lineDensity - x * 0.1);
+      ctx.lineTo(xCoord + x + j * x - i * lineDensity, yCoord+0);
       ctx.stroke();
     }
-    for (let i: number = 0; i < x / 10; i++) {
+    for (let i: number = 0; i < x / lineDensity; i++) {
       ctx.beginPath();
       ctx.strokeStyle = 'green';
-      ctx.moveTo(c + x - i * 10 - x * 0.1 + j * x, y + x);
-      ctx.lineTo(c + j * x,y + x - i * 10);
+      ctx.moveTo(xCoord + x - i * lineDensity - x * 0.1 + j * x, yCoord + x);
+      ctx.lineTo(xCoord + j * x,yCoord + x - i * lineDensity);
       ctx.stroke();
     }
   }
 }
 //row(400, 4,0,100)
 
-function linePlay(s,q){
+function linePlay(canvasSize,divider){
 
  let y: number = 0;
  let x: number = 0;
- 
- for(let r: number = 0; r<q; r++){
- row(s,q,x,y);
- y += s/q
+ let lineDensity = 1 + Math.round(((canvasSize/divider)/(canvasSize/10)))
+ for(let r: number = 0; r<divider; r++){
+ row(lineDensity,canvasSize,divider,x,y);
+ y += canvasSize/divider
 }
 
 }
