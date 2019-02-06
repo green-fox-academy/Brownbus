d875a2fb7b924000fd7e42256a270c5965bc3d5a
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-06 16:29:59 +0100

    comparable implemented, but algorythm isnt working

--- week-03/day-3/dominoes/domino.ts
+++ week-03/day-3/dominoes/domino.ts
@@ -1,8 +1,23 @@
 
-class Domino {
+  interface Comparable {
+    compareTo(other: Comparable): number;
+    }
+    
+  
+  
+class Domino implements Comparable { 
   values: number[];
   constructor(valueA: number, valueB: number) {
       this.values = [valueA, valueB];
+  }
+  compareTo(other: Domino){
+    let result: number = 1
+    if(other.values[1] === this.values[0]){
+      result = 0;
+    }else if(other.values[1] < this.values[0]){
+      result = -1;
+    }else{result = 1}
+    return result;
   }
 }
 
