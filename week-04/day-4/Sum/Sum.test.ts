d79e2d4f4a50fb47b3662cc0c3489ec9e87242a2
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-07 13:46:01 +0100

    restored the sum excercise to its original form

--- week-04/day-4/Sum/Sum.test.ts
+++ week-04/day-4/Sum/Sum.test.ts
@@ -5,9 +5,9 @@ let test = require('tape')
 
 test('Sum', t => {
   let sum1 = new Sum;
-  let testArray = ['string']
+  let testArray: number[] = [1,2,3,4,5,6]
   let actual: number = sum1.sum(testArray)
-  let expected: number = 0;
+  let expected: number = 21;
   t.equal(actual, expected)
   t.end()
 })
