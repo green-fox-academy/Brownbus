948990c46b993348b5075432bbf448c8cae6147a
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-07 13:39:50 +0100

    tested with an empty array. it failed

--- week-04/day-4/Sum/Sum.test.ts
+++ week-04/day-4/Sum/Sum.test.ts
@@ -5,7 +5,7 @@ let test = require('tape')
 
 test('Sum', t => {
   let sum1 = new Sum;
-  let testArray: number[] = [1, 2, 3, 4, 5, 6]
+  let testArray: number[] = []
   let actual: number = sum1.sum(testArray)
   let expected: number = 21;
   t.equal(actual, expected)
