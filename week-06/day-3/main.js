29f630a7309da621404c787aecd718226487e299
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-27 09:37:07 +0100

    exercise one is underway

---
+++ week-06/day-3/main.js
@@ -0,0 +1,5 @@
+let king = document.getElementById('b325');
+console.log(king);
+let businessLamp = [document.getElementsByClassName('asteroid big')];
+console.log(businessLamp[0]);
+console.log(businessLamp[1]);
