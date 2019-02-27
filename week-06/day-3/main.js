5d298c4f9760743069302823ccc52182377b364b
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-27 09:52:29 +0100

    exercise one is underway

--- week-06/day-3/main.js
+++ week-06/day-3/main.js
@@ -1,5 +1,15 @@
+//1
 let king = document.getElementById('b325');
 console.log(king);
+console.log(king.innerText)
+//2
 let businessLamp = [document.getElementsByClassName('asteroid big')];
-console.log(businessLamp[0]);
-console.log(businessLamp[1]);
+console.log(businessLamp[0][0]);
+console.log(businessLamp[0][1]);
+console.log(businessLamp[0][0].innerText);
+console.log(businessLamp[0][1].innerText)
+//3
+let conceitedKing = [king, document.getElementsByClassName("asteroid b326")]
+alert(conceitedKing[0].innerText)
+alert(conceitedKing[1][0].innerText);
+//4
