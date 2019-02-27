1c8c1649d7fd92bfbc3b26d518b007134feade1a
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-27 17:50:51 +0100

    list exercise up and down is working and just added X and thats working fine too

--- week-06/day-3/OnClick/listManager.js
+++ week-06/day-3/OnClick/listManager.js
@@ -23,7 +23,16 @@ let button1 = document.createElement('button')
 button1.setAttribute('id', 'up')
 button1.innerHTML = 'UP'
 body.appendChild(button1)
-button1.onclick = () => { }
+button1.onclick = () => { 
+  let elements = document.getElementsByTagName('li')
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].getAttribute('style') === 'background-color : blue' &&  0 < parseInt(elements[i].getAttribute('id')) ) {
+      elements[i].setAttribute('style', 'background-color : grey')
+      elements[i - 1].setAttribute('style', 'background-color : blue')
+      return;
+    }
+  }
+}
 
 let button2 = document.createElement('button')
 button2.setAttribute('id', 'down')
@@ -44,7 +53,15 @@ let button3 = document.createElement('button')
 button3.setAttribute('id', 'x')
 button3.innerHTML = 'X'
 body.appendChild(button3)
-button3.onclick = () => { }
+button3.onclick = () => {
+  let elements = document.getElementsByTagName('li')
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].getAttribute('style') === 'background-color : blue'){
+      body.removeChild(elements[i])
+      elements[0].setAttribute('style', 'background-color : blue')
+    }
+ }
+}
 
 let button4 = document.createElement('button')
 button4.setAttribute('id', 'right')
