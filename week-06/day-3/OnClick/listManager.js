a2da0a58913f61fab46b927d04d852f1dc5be3e9
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-28 10:59:52 +0100

    List manager is done and working properly, no error messages at any edge case

--- week-06/day-3/OnClick/listManager.js
+++ week-06/day-3/OnClick/listManager.js
@@ -1,34 +1,36 @@
 let body = document.querySelector('body')
+body.setAttribute('style', 'display:flex; flex-direction:row;')
+let mainUlTag = document.createElement('ul');
+mainUlTag.setAttribute('id', 'list')
+mainUlTag.setAttribute('style', 'list-style:none; border: solid 1px; padding: 0px; height: auto; width: 200px;')
+body.appendChild(mainUlTag);
+let ulTag = document.createElement('ul')
+let divTag = document.createElement('div')
+
+
 
 for (let i = 0; i < 5; i++) {
   let listElement = document.createElement('li')
   listElement.innerHTML = `listelement ${i}`
   listElement.setAttribute('id', i)
-  body.appendChild(listElement)
+  mainUlTag.appendChild(listElement)
   i === 0 ?
-    listElement.setAttribute('style', 'background-color : blue')
-    : listElement.setAttribute('style', 'background-color : grey');
+    listElement.setAttribute('style', 'background-color : blue; width : 200px; border: solid 1px;')
+    : listElement.setAttribute('style', 'background-color : grey; width : 200px; border: solid 1px;');
 }
 
 
 
-/* 
-console.log(button1)
-
-
-
-*/
-
 let button1 = document.createElement('button')
 button1.setAttribute('id', 'up')
 button1.innerHTML = 'UP'
-body.appendChild(button1)
-button1.onclick = () => { 
+divTag.appendChild(button1)
+button1.onclick = () => {
   let elements = document.getElementsByTagName('li')
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].getAttribute('style') === 'background-color : blue' &&  0 < parseInt(elements[i].getAttribute('id')) ) {
-      elements[i].setAttribute('style', 'background-color : grey')
-      elements[i - 1].setAttribute('style', 'background-color : blue')
+    if (elements[i].getAttribute('style') === 'background-color : blue; width : 200px; border: solid 1px;' && 0 <= parseInt(elements[i].getAttribute('id')) && elements[i - 1] !== undefined) {
+      elements[i].setAttribute('style', 'background-color : grey; width : 200px; border: solid 1px;')
+      elements[i - 1].setAttribute('style', 'background-color : blue; width : 200px; border: solid 1px;')
       return;
     }
   }
@@ -37,13 +39,13 @@ button1.onclick = () => {
 let button2 = document.createElement('button')
 button2.setAttribute('id', 'down')
 button2.innerHTML = 'DOWN'
-body.appendChild(button2)
+divTag.appendChild(button2)
 button2.onclick = () => {
   let elements = document.getElementsByTagName('li')
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].getAttribute('style') === 'background-color : blue' && parseInt(elements[i].getAttribute('id')) < elements.length-1) {
-      elements[i].setAttribute('style', 'background-color : grey')
-      elements[i + 1].setAttribute('style', 'background-color : blue')
+    if (elements[i].getAttribute('style') === 'background-color : blue; width : 200px; border: solid 1px;' && elements.length > elements.length - 1 && elements[i + 1] !== undefined) {
+      elements[i].setAttribute('style', 'background-color : grey; width : 200px; border: solid 1px;; width : 200px; border: solid 1px;')
+      elements[i + 1].setAttribute('style', 'background-color : blue; width : 200px; border: solid 1px;')
       return;
     }
   }
@@ -52,19 +54,51 @@ button2.onclick = () => {
 let button3 = document.createElement('button')
 button3.setAttribute('id', 'x')
 button3.innerHTML = 'X'
-body.appendChild(button3)
+divTag.appendChild(button3)
 button3.onclick = () => {
   let elements = document.getElementsByTagName('li')
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].getAttribute('style') === 'background-color : blue'){
-      body.removeChild(elements[i])
-      elements[0].setAttribute('style', 'background-color : blue')
+    if (elements[i].getAttribute('style') === 'background-color : blue; width : 200px; border: solid 1px;' && mainUlTag.hasChildNodes() == true) {
+      mainUlTag.removeChild(elements[i])
+      if (elements[0] !== undefined) {
+        elements[0].setAttribute('style', 'background-color : blue; width : 200px; border: solid 1px;')
+      }
     }
- }
+  }
 }
+
+
+
 
 let button4 = document.createElement('button')
 button4.setAttribute('id', 'right')
 button4.innerHTML = '>'
-body.appendChild(button4)
-button4.onclick = () => { }
+divTag.appendChild(button4)
+button4.onclick = () => {
+  let elements = document.getElementsByTagName('li')
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].getAttribute('style') === 'background-color : blue; width : 200px; border: solid 1px;') {
+      elements[i].setAttribute('style', 'background-color : grey; width : 200px; border: solid 1px;')
+      ulTag.appendChild(elements[i])
+
+      if (elements[0] !== undefined) {
+        elements[0].setAttribute('style', 'background-color : blue; width : 200px; border: solid 1px;')
+      }
+    }
+  }
+}
+
+body.appendChild(divTag)
+body.appendChild(ulTag);
+
+
+let buttons = document.getElementsByTagName('button')
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].setAttribute('style', 'background-color: grey; width: 200px; height: 25px; border: solid 2px; border-color: blue; border-radius: 5%')
+}
+
+let styleTag = document.createElement('style')
+styleTag.innerHTML = 'div { \n display:flex; \n flex-direction:column; \n margin-top: 16px;}'
+body.appendChild(styleTag)
+
+ulTag.setAttribute('style', 'display:flex; flex-direction:column; height:auto; width: 200px; border:solid 1px; list-style:none;padding:0px;  ')
