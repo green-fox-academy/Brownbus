59cd37790bd9da945ebbb6b633c12683587cfed7
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-02-28 17:28:49 +0100

    Greeting exercise is done and working

---
+++ week-06/day-4/Greetings/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  let name = req.query.name;
+  if (name == undefined) {
+    name = 'Guest'
+  } else {
+    name = req.query.name
+  }
+  res.render('home', {
+    title: ` Hello ${name}!`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
