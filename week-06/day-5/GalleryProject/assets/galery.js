59d28dfac3e1f8a7902a79915776211db99328a5
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-03-03 17:49:51 +0100

    Gallery is working and the files now appear at the slides after uploaded

--- week-06/day-5/GalleryProject/assets/galery.js
+++ week-06/day-5/GalleryProject/assets/galery.js
@@ -2,26 +2,75 @@
 let buttonLeft = document.getElementById('leftButton');
 let buttonRight = document.getElementById('rightButton');
 let galery = document.getElementById('gallery')
-let selectedImage = document.querySelector('img')
+let selectedImage = document.getElementById('selected')
 let ulTag = document.querySelector('ul')
 let body = document.querySelector('body')
+let submit = document.getElementById('sumbit')
 document.body.addEventListener('keydown', onKeyPress);
+
+//BROWSERIFY INSTALLED
+const fs = require('browserify-fs');
+//BROWSERIFY INSTALLED
+
+const dir = './assets/pictures';
+
+fs.readdir(dir, (err, files) => {
+  console.log(files.length);
+});
+
 
 
 let counter = 0;
 let numberOfThumbs = 10;
 
+
+
+function thumnailLoader(){
+  
+  while (ulTag.hasChildNodes()){
+    ulTag.removeChild(ulTag.lastChild);
+  } 
+  for (let i = 0; i < numberOfThumbs; i++) {
+    let liTag = document.createElement('li')
+    liTag.setAttribute('id', `ID${i}`)
+    liTag.setAttribute('onclick', `{selectedImage.setAttribute('src', './assets/pictures/${i}.jpg'); counter = ${i}};`)
+    let imgTag = document.createElement('img')
+    imgTag.setAttribute('src', `./assets/pictures/${i}.jpg`)
+    imgTag.setAttribute('class', `thumb`)
+    imgTag.setAttribute('id', `thumb${i}`)
+    liTag.appendChild(imgTag)
+    ulTag.appendChild(liTag)
+  }
+}  
+
+
+function adder(){
+  numberOfThumbs += 1;
+}
+
+thumnailLoader()
+
+/* for (let i = 0; i < numberOfThumbs; i++) {
+  scriptTag.innerHTML +=` thumb${i}.onclick = () => {selectedImage.setAttribute('src', './assets/pictures/${i}.jpg'); console.log('${i}')};`
+} */
+
 function right() {
+  if (counter < numberOfThumbs-1) {
+  counter++
+  }
   selectedImage.setAttribute('src', `./assets/pictures/${counter}.jpg`)
-  if (counter < 9) {
-    counter++
+  if (counter < numberOfThumbs-1) {
+    thumnailLoader()
   }
 }
 
 function left() {
+  if(counter > 0){
+    counter--
+  }
   selectedImage.setAttribute('src', `./assets/pictures/${counter}.jpg`)
   if (counter > 0) {
-    counter--
+    thumnailLoader()
   }
 }
 buttonLeft.onclick = () => {
@@ -32,33 +81,16 @@ buttonRight.onclick = () => {
   right()
 }
 
-for(let i = 0; i < numberOfThumbs; i++){
-let liTag =document.createElement('li')
-liTag.setAttribute('id', `ID${i}`)
-let imgTag =document.createElement('img')
-imgTag.setAttribute('src', `./assets/pictures/${i}.jpg` )
-imgTag.setAttribute('class', `thumb` )
-imgTag.setAttribute('id', `thumb${i}` )
-liTag.appendChild(imgTag)
-ulTag.appendChild(liTag)
-}
 
-let scriptTag = document.createElement('script')
-for(let i = 0; i < numberOfThumbs; i++){
-  scriptTag.innerHTML += `\n thumb${i} = document.getElementById('thumb${i}'); \n thumb${i}.onclick = () => {selectedImage.setAttribute('src', './assets/pictures/${i}.jpg'); counter = ${i}};  \n`
-}
-
-body.appendChild(scriptTag)
-
-
-/* `<img src='./assets/pictures/0.jpg' alt="this is an image" class="thumb" id="thumb1"/>` */
 
 function onKeyPress(event) {
-switch (event.keyCode) {
+ 
+  switch (event.keyCode) {
     case 37:
-    left();
-    break;
+      left();
+      break;
     case 39:
-    right();
-    break;
-}}
+      right();
+      break;
+  }
+}
--- week-06/day-5/GalleryProject/index.js
+++ week-06/day-5/GalleryProject/index.js
@@ -1,28 +1,78 @@
 'use strict';
 
+
 const express = require('express');
+const multer = require('multer'); // file storing middleware
+const bodyParser = require('body-parser'); //cleans our req.body
 const app = express();
 const PORT = 3000;
-let path = require('path')
+const path = require('path')
+//let numberedThumbs = require('./assets/galery.js')
+let counter = 10;
 
+app.use(bodyParser.urlencoded({ extended: false })); //handle body requests
+app.use(bodyParser.json()); // let's make JSON work too!
+
+//app.use('/', express.static(__dirname + '/assets')); 
 app.use('/assets', express.static('assets'))
 app.use('/assets/pictures', express.static('pictures'))
 
+// FOR IMAGE SAVING
 
+const multerConfig = {
 
+  storage: multer.diskStorage({
+    //Setup where the user's file will go
+    destination: function (req, file, next) {
+      next(null, './assets/pictures');
+    },
+
+    //Then give the file a unique name
+    filename: function (req, file, next) {
+      console.log(file);
+      const ext = file.mimetype.split('/')[1];
+      next(null,`${counter}` + '.' + 'jpg');
+    }
+  }),
+
+  //A means of ensuring only images are uploaded. 
+  fileFilter: function (req, file, next) {
+    if (!file) {
+      next();
+    }
+    const image = file.mimetype.startsWith('image/');
+    if (image) {
+      console.log('photo uploaded');
+      next(null, true);
+    } else {
+      console.log("file not supported");
+
+      //TODO:  A better message response to user on failure.
+      return next();
+    }
+  }
+};
+/////////////////////////////////////////////////
 
 app.set('view engine', 'ejs');
 
 
-  app.get('/', (req, res) => {
-    res.render('home', {
-      img: `<img src='./assets/pictures/0.jpg' alt="this is an image" id="selected"/>`,
-      thumbnail: `<ul></ul>`
-      })
-    })
-  
+app.get('/', (req, res) => {
+  res.render('home', {
+    img: `<img src='./assets/pictures/0.jpg' alt="this is an image" id="selected"/>`,
+    thumbnail: `<ul></ul>`
+  })
+})
 
-  
+//route2
+ app.post('/upload',  multer(multerConfig).single('photo'),  function (req, res) {
+  /* res.send('Complete!'); */
+  counter += 1;
+  console.log('server:', counter)
+}); 
+
+
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
