8e13e9c2f429e48bfb52fabcb91e6b2b5eb0a390
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-03-20 11:43:07 +0100

    the ffil or not ffill exercise id done

--- week-09/day-2/promise/promise2/somethingelse.js
+++ week-09/day-2/promise/promise2/somethingelse.js
@@ -1,4 +1,4 @@
-
+/* 
 'use strict';
 
 let promise = new Promise(function (fulfill, reject) {
@@ -15,4 +15,32 @@ function onReject(myRrror) {
   return myRrror
 }
 
-promise.then(string => string.json(), errorMsg => console.log(errorMsg.message))
+promise.then(string => string.json(), errorMsg => console.log(errorMsg.message)) */
+
+
+ /*
+     * This code is bad, but nonetheless common and has the nasty result of
+     * calling the supplied callback more than once (possibly destroying the
+     * earth?). It is conventional to return the first invocation of callback
+     * but it’s easy to overlook!
+     */
+
+
+ /*    'use strict';
+
+    let promise = new Promise(function (fulfill, reject) {
+     fulfill('I FIRED')
+     reject(new Error('I DID NOT FIRE'))
+    });
+    
+    function onRejected(error){
+      console.log(error.message)
+    }
+    promise.then(console.log, onRejected) */
+
+    'use strict';
+
+    let promise = new Promise(function (fulfill, reject) {
+     fulfill('I FIRED')
+     reject(new Error('I DID NOT FIRE'))
+    });
