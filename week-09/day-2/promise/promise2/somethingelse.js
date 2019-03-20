347146c18bd3b5c9041e71bde84143318933eafd
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-03-20 11:34:20 +0100

    added the promsie rejection thingy exercise

--- week-09/day-2/promise/promise2/somethingelse.js
+++ week-09/day-2/promise/promise2/somethingelse.js
@@ -1,15 +1,18 @@
 
-    'use strict';
+'use strict';
 
-    let promise = new Promise(function (fulfill, reject) {
-      // Your solution here
-      setTimeout(()=>{
-        fulfill('FULFILLED!');
-      }, 300);
-    });
-    
-    promise.then(function(value) {
-      console.log(value);
-    });
-    
+let promise = new Promise(function (fulfill, reject) {
+  let MyError = new Error('REJECTED!');
+  setTimeout(() => {
+    if(1===2){
+      fulfill(console.log('anyád'))
+    }
+    reject(onReject(MyError))
+  }, 300);
+});
 
+function onReject(myRrror) {
+  return myRrror
+}
+
+promise.then(string => string.json(), errorMsg => console.log(errorMsg.message))
